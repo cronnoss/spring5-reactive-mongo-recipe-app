4195ca7bed481c91c66eac70e287097684c48e95
Evgeniy Karasev <karasevevg@gmail.com>
2021-12-10 23:31:00 +0300

    15. Introduction to Spring WebTestClient

---
+++ src/test/java/com/cronnoss/controllers/RouterFunctionTest.java
@@ -0,0 +1,58 @@
+package com.cronnoss.controllers;
+
+import com.cronnoss.config.WebConfig;
+import com.cronnoss.domain.Recipe;
+import com.cronnoss.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import reactor.core.publisher.Flux;
+
+import static org.mockito.Mockito.when;
+
+
+public class RouterFunctionTest {
+
+    WebTestClient webTestClient;
+
+    @Mock
+    RecipeService recipeService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        WebConfig webConfig = new WebConfig();
+
+        RouterFunction<?> routerFunction = webConfig.routes(recipeService);
+
+        webTestClient = WebTestClient.bindToRouterFunction(routerFunction).build();
+    }
+
+    @Test
+    void testGetRecipes() {
+
+        when(recipeService.getRecipes()).thenReturn(Flux.just());
+
+        webTestClient.get().uri("/api/recipes")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
+    @Test
+    void testGetRecipesWithData() {
+
+        when(recipeService.getRecipes()).thenReturn(Flux.just(new Recipe(), new Recipe()));
+
+        webTestClient.get().uri("/api/recipes")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBodyList(Recipe.class);
+    }
+}
